7d0c3c6486f37eed3c54b3f73a48d12cb3e277da
Jinbao Chen <cjinbao@vmware.com>
2020-10-14 17:04:53 +0800

    Use a fixed-length FileSegInfo* Array in AppendOnlyFetchDesc
    The segmentFileInfo in AppendOnlyFetchDesc is a compact list before,
    But we need use a loop to find the FileSegInfo, This can lead to
    inefficiency in some cases. So this patch use a fixed-length
    FileSegInfo* Array to instead.

--- src/backend/access/appendonly/appendonlyam.c
+++ src/backend/access/appendonly/appendonlyam.c
@@ -1814,39 +1814,22 @@ static bool
 openFetchSegmentFile(AppendOnlyFetchDesc aoFetchDesc,
 					 int openSegmentFileNum)
 {
-	int			i;
-
 	FileSegInfo *fsInfo;
-	int			segmentFileNum;
 	int64		logicalEof;
 	int32		fileSegNo;
 
 	Assert(!aoFetchDesc->currentSegmentFile.isOpen);
 
-	i = 0;
-	while (true)
+	fsInfo = aoFetchDesc->segmentFileInfo[openSegmentFileNum];
+	if (fsInfo->state == AOSEG_STATE_AWAITING_DROP)
 	{
-		if (i >= aoFetchDesc->totalSegfiles)
-			return false;
-		/* Segment file not visible in catalog information. */
-
-		fsInfo = aoFetchDesc->segmentFileInfo[i];
-		segmentFileNum = fsInfo->segno;
-		if (openSegmentFileNum == segmentFileNum)
-		{
-			if (fsInfo->state == AOSEG_STATE_AWAITING_DROP)
-			{
-				/*
-				 * File compacted, but not dropped. All information are
-				 * declared invisible
-				 */
-				return false;
-			}
-			logicalEof = (int64) fsInfo->eof;
-			break;
-		}
-		i++;
+		/*
+		 * File compacted, but not dropped. All information are
+		 * declared invisible
+		 */
+		return false;
 	}
+	logicalEof = (int64) fsInfo->eof;
 
 	/*
 	 * Don't try to open a segment file when its EOF is 0, since the file may
@@ -2151,7 +2134,7 @@ appendonly_fetch_init(Relation relation,
 	 * Get information about all the file segments we need to scan
 	 */
 	aoFetchDesc->segmentFileInfo =
-		GetAllFileSegInfo(
+		GetAllFileSegInfoArray(
 						  relation,
 						  appendOnlyMetaDataSnapshot,
 						  &aoFetchDesc->totalSegfiles,
@@ -2169,6 +2152,7 @@ appendonly_fetch_init(Relation relation,
 		segno = (i < 0 ? 0 : aoFetchDesc->segmentFileInfo[i]->segno);
 		/* set corresponding bit for target segment */
 		aoFetchDesc->lastSequence[segno] = ReadLastSequence(aoFormData.segrelid, segno);
+		aoFetchDesc->firstRowNum[segno] = AOTupleId_MaxRowNum;
 	}
 
 	AppendOnlyStorageRead_Init(
@@ -2211,7 +2195,6 @@ appendonly_fetch_init(Relation relation,
 											&aoFetchDesc->blockDirectory,
 											appendOnlyMetaDataSnapshot,
 											aoFetchDesc->segmentFileInfo,
-											aoFetchDesc->totalSegfiles,
 											aoFetchDesc->relation,
 											1,
 											false,
@@ -2465,7 +2448,7 @@ appendonly_fetch_finish(AppendOnlyFetchDesc aoFetchDesc)
 
 	if (aoFetchDesc->segmentFileInfo)
 	{
-		FreeAllSegFileInfo(aoFetchDesc->segmentFileInfo, aoFetchDesc->totalSegfiles);
+		FreeAllSegFileInfoArray(aoFetchDesc->segmentFileInfo);
 		pfree(aoFetchDesc->segmentFileInfo);
 		aoFetchDesc->segmentFileInfo = NULL;
 	}
--- src/include/cdb/cdbappendonlyblockdirectory.h
+++ src/include/cdb/cdbappendonlyblockdirectory.h
@@ -97,7 +97,6 @@ typedef struct AppendOnlyBlockDirectory
 
 	MemoryContext memoryContext;
 	
-	int				totalSegfiles;
 	FileSegInfo 	**segmentFileInfo;
 
 	/*
@@ -186,7 +185,6 @@ extern void AppendOnlyBlockDirectory_Init_forSearch(
 	AppendOnlyBlockDirectory *blockDirectory,
 	Snapshot appendOnlyMetaDataSnapshot,
 	FileSegInfo **segmentFileInfo,
-	int totalSegfiles,
 	Relation aoRel,
 	int numColumnGroups,
 	bool isAOCol,
