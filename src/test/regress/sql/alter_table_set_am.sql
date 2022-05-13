7b1aed1cd90a96b90bb307acc67d88cfb9ec3d8a
Soumyadeep Chakraborty <soumyadeep2007@gmail.com>
2022-05-13 16:20:27 -0700

    AT SET WITH() syntax to change table access method
    AT SET WITH syntax was used only for reorganize set dist
    
    This commit adds support to change access method of the table using that
    syntax.
    ALTER TABLE mp_heap_table SET WITH (appendoptimized=true);
    
    To support the AT SET WITH syntax variant, we use a thin layer over the
    ATSETAM infrastructure, handling most of the massaging in the grammar.
    
    We currently do not support both reorganize and changing access method
    in the same AT command, since it currently requires two rewrites of the
    table.
    AT SET WITH() can now be used to either reorganize or for changing
    access method.
    
    Co-authored-by: Divyesh Vanjare <vanjared@vmware.com>

--- src/test/regress/sql/alter_table_set_am.sql
+++ src/test/regress/sql/alter_table_set_am.sql
@@ -2,22 +2,25 @@
 
 -- Scenario 1: Heap to Heap
 CREATE TABLE heap2heap(a int, b int) DISTRIBUTED BY (a);
+CREATE TABLE heap2heap2(a int, b int) DISTRIBUTED BY (a);
 
 INSERT INTO heap2heap SELECT i,i FROM generate_series(1,5) i;
+INSERT INTO heap2heap2 SELECT i,i FROM generate_series(1,5) i;
 
 CREATE TEMP TABLE relfilebeforeheap AS
-    SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('heap2heap')
-    UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
-    WHERE relname in ('heap2heap') ORDER BY segid;
+SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('heap2heap', 'heap2heap2')
+UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
+WHERE relname in ('heap2heap', 'heap2heap2') ORDER BY segid;
 
 -- changing to the same access method shouldn't rewrite the table
 -- (i.e. the relfilenodes shouldn't change)
 ALTER TABLE heap2heap SET ACCESS METHOD heap;
+ALTER TABLE heap2heap2 SET WITH (appendoptimized=false);
 
 CREATE TEMP TABLE relfileafterheap AS
-    SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('heap2heap')
-    UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
-    WHERE relname in ('heap2heap') ORDER BY segid;
+SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('heap2heap', 'heap2heap2')
+UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
+WHERE relname in ('heap2heap', 'heap2heap2') ORDER BY segid;
 
 -- relfilenodes shouldn't change
 SELECT count(*) FROM (SELECT * FROM relfilebeforeheap UNION SELECT * FROM relfileafterheap)a;
@@ -34,9 +37,9 @@ INSERT INTO heap2ao SELECT i,i FROM generate_series(1,5) i;
 INSERT INTO heap2ao2 SELECT i,i FROM generate_series(1,5) i;
 
 CREATE TEMP TABLE relfilebeforeao AS
-    SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('heap2ao', 'heap2ao2', 'heapi')
-    UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
-    WHERE relname in ('heap2ao', 'heap2ao2', 'heapi') ORDER BY segid;
+SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('heap2ao', 'heap2ao2', 'heapi')
+UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
+WHERE relname in ('heap2ao', 'heap2ao2', 'heapi') ORDER BY segid;
 
 -- Altering a heap table with a unique index to AO should error out
 -- as unique indexes aren't supported on AO tables
@@ -48,7 +51,7 @@ SET gp_default_storage_options = 'blocksize=65536, compresstype=zlib, compressle
 
 -- Alter table heap to AO should work
 ALTER TABLE heap2ao SET ACCESS METHOD ao_row;
-ALTER TABLE heap2ao2 SET ACCESS METHOD ao_row;
+ALTER TABLE heap2ao2 SET WITH (appendoptimized=true);
 
 -- The altered table should inherit storage options from gp_default_storage_options
 SELECT blocksize,compresslevel,checksum,compresstype,columnstore
@@ -62,9 +65,9 @@ SELECT * FROM heap2ao2;
 
 -- The tables and indexes should have been rewritten (should have different relfilenodes)
 CREATE TEMP TABLE relfileafterao AS
-    SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('heap2ao', 'heap2ao2', 'heapi')
-    UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
-    WHERE relname in ('heap2ao', 'heap2ao2', 'heapi') ORDER BY segid;
+SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('heap2ao', 'heap2ao2', 'heapi')
+UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
+WHERE relname in ('heap2ao', 'heap2ao2', 'heapi') ORDER BY segid;
 
 SELECT * FROM relfilebeforeao INTERSECT SELECT * FROM relfileafterao;
 
@@ -82,54 +85,71 @@ DROP TABLE heap2ao2;
 -- check inherited tables
 CREATE TABLE heapbase (a int, b int);
 CREATE TABLE child (c int) INHERITS (heapbase);
+CREATE TABLE heapbase2 (a int, b int);
+CREATE TABLE child2 (c int) INHERITS (heapbase2);
 
 INSERT INTO heapbase SELECT i,i FROM generate_series(1,5) i;
 INSERT INTO child SELECT i,i,i FROM generate_series(1,5) i;
+INSERT INTO heapbase2 SELECT i,i FROM generate_series(1,5) i;
+INSERT INTO child2 SELECT i,i,i FROM generate_series(1,5) i;
 
 CREATE TEMP TABLE inheritrelfilebefore AS
-    SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('heapbase')
-    UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
-    WHERE relname in ('heapbase') ORDER BY segid;
+SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('heapbase', 'heapbase2')
+UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
+WHERE relname in ('heapbase', 'heapbase2') ORDER BY segid;
 
 CREATE TEMP TABLE inheritchildrelfilebefore AS
-    SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('child')
-    UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
-    WHERE relname in ('child') ORDER BY segid;
+SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('child', 'child2')
+UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
+WHERE relname in ('child', 'child2') ORDER BY segid;
 
 ALTER TABLE heapbase SET ACCESS METHOD ao_row;
+ALTER TABLE heapbase2 SET WITH (appendoptimized=true);
 
 -- The altered table should inherit storage options from gp_default_storage_options
 show gp_default_storage_options;
 SELECT blocksize,compresslevel,checksum,compresstype,columnstore
 FROM pg_appendonly WHERE relid='heapbase'::regclass::oid;
+
+SELECT blocksize,compresslevel,checksum,compresstype,columnstore
+FROM pg_appendonly WHERE relid='heapbase2'::regclass::oid;
+
 SELECT blocksize,compresslevel,checksum,compresstype,columnstore
 FROM pg_appendonly WHERE relid='child'::regclass::oid;
+SELECT blocksize,compresslevel,checksum,compresstype,columnstore
+FROM pg_appendonly WHERE relid='child2'::regclass::oid;
 
 -- Check data is intact
 SELECT * FROM heapbase;
+SELECT * FROM heapbase2;
 
 -- relfile node should change for base table set to AO
 CREATE TEMP TABLE inheritrelfileafter AS
-    SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('heapbase')
-    UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
-    WHERE relname in ('heapbase') ORDER BY segid;
+SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('heapbase', 'heapbase2')
+UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
+WHERE relname in ('heapbase', 'heapbase2') ORDER BY segid;
 
 SELECT * FROM inheritrelfilebefore INTERSECT SELECT * FROM inheritrelfileafter;
 
 -- relfile node should not change for child table
 CREATE TEMP TABLE inheritchildrelfileafter AS
-    SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('child')
-    UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
-    WHERE relname in ('child') ORDER BY segid;
+SELECT -1 segid, relfilenode FROM pg_class WHERE relname in ('child', 'child2')
+UNION SELECT gp_segment_id segid, relfilenode FROM gp_dist_random('pg_class')
+WHERE relname in ('child', 'child2') ORDER BY segid;
 
 SELECT count(*) FROM (SELECT * FROM inheritchildrelfilebefore UNION SELECT * FROM inheritchildrelfileafter)a;
 
 -- aux tables are created, pg_appendonly row is created
 SELECT * FROM gp_toolkit.__gp_aoseg('heapbase');
 SELECT * FROM gp_toolkit.__gp_aovisimap('heapbase');
+SELECT * FROM gp_toolkit.__gp_aoseg('heapbase2');
+SELECT * FROM gp_toolkit.__gp_aovisimap('heapbase2');
 
 -- aux tables are not created for child table
 SELECT * FROM gp_toolkit.__gp_aoseg('child');
 SELECT * FROM gp_toolkit.__gp_aovisimap('child');
+SELECT * FROM gp_toolkit.__gp_aoseg('child2');
+SELECT * FROM gp_toolkit.__gp_aovisimap('child2');
 
 DROP TABLE heapbase CASCADE;
+DROP TABLE heapbase2 CASCADE;
