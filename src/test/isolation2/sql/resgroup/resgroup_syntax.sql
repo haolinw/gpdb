05d717fab197cb92c5bc602dcb6b5adc3fb28bfd
RMT <d.rong@outlook.com>
2023-05-31 17:32:41 +0800

    adjust test cases

--- src/test/isolation2/sql/resgroup/resgroup_syntax.sql
+++ src/test/isolation2/sql/resgroup/resgroup_syntax.sql
@@ -238,3 +238,23 @@ SELECT groupname,cpu_hard_quota_limit,cpuset FROM gp_toolkit.gp_resgroup_config 
 ALTER RESOURCE GROUP rg_test_group SET cpu_hard_quota_limit 10;
 SELECT groupname,cpu_hard_quota_limit,cpuset FROM gp_toolkit.gp_resgroup_config WHERE groupname='rg_test_group';
 DROP RESOURCE GROUP rg_test_group;
+
+-- positive: io limit with correct syntax
+CREATE RESOURCE GROUP rg_test_group WITH (concurrency=10, cpu_hard_quota_limit=10, io_limit='pg_default:rbps=1000,wbps=1000,riops=1000,wiops=1000');
+DROP RESOURCE GROUP rg_test_group;
+
+-- negative: io limit with incorrect syntax
+-- * must be unique tablespace
+CREATE RESOURCE GROUP rg_test_group WITH (concurrency=10, cpu_hard_quota_limit=10, io_limit='pg_default:rbps=1000,wbps=1000,riops=1000,wiops=1000;*:wbps=1000');
+
+-- tail ;
+CREATE RESOURCE GROUP rg_test_group WITH (concurrency=10, cpu_hard_quota_limit=10, io_limit='pg_default:rbps=1000,wbps=1000,riops=1000,wiops=1000;');
+
+-- tail ,
+CREATE RESOURCE GROUP rg_test_group WITH (concurrency=10, cpu_hard_quota_limit=10, io_limit='pg_default:rbps=1000,wbps=1000,riops=1000,');
+
+-- with space
+CREATE RESOURCE GROUP rg_test_group WITH (concurrency=10, cpu_hard_quota_limit=10, io_limit=' pg_default:rbps=1000,wbps=1000,riops=1000,wiops=1000');
+
+-- with space
+CREATE RESOURCE GROUP rg_test_group WITH (concurrency=10, cpu_hard_quota_limit=10, io_limit='pg_default:rbps=1000, wbps=1000,riops=1000,wiops=1000');
