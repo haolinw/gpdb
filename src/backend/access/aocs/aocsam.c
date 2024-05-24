1cf2465f2eb22a11ab013551eb6d78c9c1db0e21
Haolin Wang <whaolin@vmware.com>
2024-05-24 09:27:23 +0800

    suppress compiler warning for assert-only variable

--- src/backend/access/aocs/aocsam.c
+++ src/backend/access/aocs/aocsam.c
@@ -2253,7 +2253,7 @@ aocs_fetch(AOCSFetchDesc aocsFetchDesc,
 {
 	int			segmentFileNum = AOTupleIdGet_segmentFileNum(aoTupleId);
 	int64		rowNum = AOTupleIdGet_rowNum(aoTupleId);
-	int			numCols = aocsFetchDesc->relation->rd_att->natts;
+	int			numCols PG_USED_FOR_ASSERTS_ONLY = aocsFetchDesc->relation->rd_att->natts;
 	int			colno = 0; /* initialize to placate compiler, but it should be overwritten later in any case */
 	bool		found = true;
 	bool		isSnapshotAny = (aocsFetchDesc->snapshot == SnapshotAny);
